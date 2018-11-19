193460f132928a0644a617f1662b0a0d053c3629
yichichou <30364736+yichichou@users.noreply.github.com>
2018-11-19 15:05:25 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -9,36 +9,17 @@
   $sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
   $sender_replyToken = $json_obj->events[0]->replyToken; //取得訊息的replyToken
   
-  $sender_txt=rawurlencode($sender_txt); //因為使用get的方式呼叫luis api，所以需要轉碼
-  $ch = curl_init('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/7cfa8e0f-7e8d-44c8-945d-ed362856b5fc?subscription-key=ab9e8942776443a497599063db3b7b44&timezoneOffset=-360&q='.$sender_txt);                                                                      
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");                                                                                                                          
+  $imageId = $json_obj->events[0]->message->id; //取得訊息編號
+  $url = 'https://api.line.me/v2/bot/message/'.$imageId.'/content';
+  $ch = curl_init($url);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  $result_str = curl_exec($ch);
-  fwrite($myfile, "\xEF\xBB\xBF".$result_str); //在字串前加上\xEF\xBB\xBF轉成utf8格式
-  $result = json_decode($result_str);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Authorization: Bearer f9J+SIcrrv+iLO/NmtQrJNaLYJZ+EKIrRm/G3c7HD9V1xij+L39EZNWwzXNgl7yM0g/fFlbFXqzOwPBurRuuytkEhRgoboIBASmDphR3Z8ORK02HZhZ4e3YOkaetKj5LY2XzuuBDuKiv4yuqv/e4yAdB04t89/1O/w1cDnyilFU='
+  ));
 
-  //將這object分數最高的intent選擇出來
-  $ans_txt = $result -> topScoringIntent -> intent;
-  $response = array (
-    "to" => $sender_userid,
-    "messages" => array (
-      array (
-        "type" => "text",
-        "text" => $ans_txt
-      )
-    )
-  );
-  
-  
- fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
-  $header[] = "Content-Type: application/json";
-  //line developer: "channel access token"
-  $header[] = "Authorization: Bearer f9J+SIcrrv+iLO/NmtQrJNaLYJZ+EKIrRm/G3c7HD9V1xij+L39EZNWwzXNgl7yM0g/fFlbFXqzOwPBurRuuytkEhRgoboIBASmDphR3Z8ORK02HZhZ4e3YOkaetKj5LY2XzuuBDuKiv4yuqv/e4yAdB04t89/1O/w1cDnyilFU=";
-  $ch = curl_init("https://api.line.me/v2/bot/message/push");
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
-  $result = curl_exec($ch);
+  $json_content = curl_exec($ch);
   curl_close($ch);
+  $imagefile = fopen($imageId.".jpeg", "w+") or die("Unable to open file!");
+  fwrite($imagefile, $json_content); 
+  fclose($imagefile); //將圖片存在server上
 ?>
