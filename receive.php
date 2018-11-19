1d29e7eb0cc8269bcb34a8a803d015676f947522
yichichou <30364736+yichichou@users.noreply.github.com>
2018-11-19 16:29:12 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -9,72 +9,78 @@
   $sender_txt = $json_obj->events[0]->message->text; //取得訊息內容
   $sender_replyToken = $json_obj->events[0]->replyToken; //取得訊息的replyToken
   
-  $imageId = $json_obj->events[0]->message->id; //取得訊息編號
-  $url = 'https://api.line.me/v2/bot/message/'.$imageId.'/content';
-  $ch = curl_init($url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Authorization: Bearer f9J+SIcrrv+iLO/NmtQrJNaLYJZ+EKIrRm/G3c7HD9V1xij+L39EZNWwzXNgl7yM0g/fFlbFXqzOwPBurRuuytkEhRgoboIBASmDphR3Z8ORK02HZhZ4e3YOkaetKj5LY2XzuuBDuKiv4yuqv/e4yAdB04t89/1O/w1cDnyilFU='
-  ));
-
-  $json_content = curl_exec($ch);
-  curl_close($ch);
-  $imagefile = fopen($imageId.".jpeg", "w+") or die("Unable to open file!");
-  fwrite($imagefile, $json_content); 
-  fclose($imagefile); //將圖片存在server上
-  //先在line，上傳一張圖片
-  //在瀏覽器中輸入: https://sporzfy.com/class/20181119_line/log.txt，會在裏頭顯示image type的id
-  //可以在網址上輸入: https://sporzfy.com/class/20181119_line/8884441406951.jpeg (8884441406951是image type的id )
-  
-
-
-  $header[] = "Content-Type: application/json";
-  $post_data = array (
-	"requests" => array (
-	  array (
-		"image" => array (
-		  "source" => array (
-			"imageUri" => "http://139.59.123.8/class/20181119_line/".$imageId.".jpeg"  //20181119_line是jenkins的專案
-		  )
-		),
-		"features" => array (
-		  array (
-			"type" => "TEXT_DETECTION",
-			"maxResults" => 1
-		  )
-		)
-	  )
-	)
-  );
-
-  //google vision api的金鑰
-  $ch = curl_init('https://vision.googleapis.com/v1/images:annotate?key=AIzaSyCiyGiCfjzzPR1JS8PrAxcsQWHdbycVwmg');                                                                      
-  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-  curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));                                                                  
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-  curl_setopt($ch, CURLOPT_HTTPHEADER, $header);                                                                                                   
-  $result = json_decode(curl_exec($ch));
-  $result_ary = mb_split("\n",$result -> responses[0] -> fullTextAnnotation -> text);
-  fwrite($myfile, "\xEF\xBB\xBF".$result -> responses[0] -> fullTextAnnotation -> text);
-  $ans_txt = "這張發票沒用了，你又製造了一張垃圾";
-
-  foreach ($result_ary as $val) {
-	if($val == "JS-07510912"){
-	  $ans_txt = "恭喜您中獎啦，快分紅!!";
-	}
+  switch ($sender_txt) {
+    case "請告訴我妳的名字":  //圖文選單中連結欄位的文字
+			$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "text",
+						"text" => "Hello. This is push. You say ".$sender_txt
+					)
+				)
+			);
+      break;
+    case "想要吃甚麼":
+			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
+			$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "location",
+						"title" => "my location",
+						"address" => "〒150-0002 東京都渋谷区渋谷２丁目２１−１",
+						"latitude" => 35.65910807942215,
+						"longitude" => 139.70372892916203
+					)
+				)
+			);
+			break;
+		case "你喜歡玩甚麼":
+			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
+			$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "sticker",
+						"packageId" => "1",
+						"stickerId" => "1"
+					)
+				)
+			);
+			break;
+		case "你要從哪出發":
+			$line_server_url = 'https://api.line.me/v2/bot/message/reply';
+			$response = array (
+				"replyToken" => $sender_replyToken,
+				"messages" => array (
+					array (
+						"type" => "template",
+						"altText" => "this is a buttons template",
+						"template" => array (
+							"type" => "buttons",
+							"thumbnailImageUrl" => "https://www.w3schools.com/css/paris.jpg",
+							"title" => "Menu",
+							"text" => "Please select",
+							"actions" => array (
+								array (
+									"type" => "postback",
+									"label" => "Buy",
+									"data" => "action=buy&itemid=123"
+								),
+								array (
+									"type" => "postback",
+									"label" => "Add to cart",
+									"data" => "action=add&itemid=123"
+								)
+							)
+						)
+					)
+				)
+			);
+			break;
   }
-  $response = array (
-	"replyToken" => $sender_replyToken,
-	"messages" => array (
-	  array (
-		"type" => "text",
-		//"text" => $ans_txt
-		"text" => $result -> responses[0] -> fullTextAnnotation -> text  //顯示所有在line上傳圖片的中文字
-	  )
-	)
-  );
-  
-  
+
   fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
   $header[] = "Content-Type: application/json";
   $header[] = "Authorization: Bearer f9J+SIcrrv+iLO/NmtQrJNaLYJZ+EKIrRm/G3c7HD9V1xij+L39EZNWwzXNgl7yM0g/fFlbFXqzOwPBurRuuytkEhRgoboIBASmDphR3Z8ORK02HZhZ4e3YOkaetKj5LY2XzuuBDuKiv4yuqv/e4yAdB04t89/1O/w1cDnyilFU=";
